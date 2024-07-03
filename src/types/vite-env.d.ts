2f56b33deff27e67a39819909d78435c77c50898
Jacob <jacobshi777@hotmail.com>
2024-07-03 22:29:01 +0800

    feat: favicon.ico

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,11 +1,12 @@
-// src/vite-env.d.ts or types/vite-env.d.ts
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
   readonly VITE_PROXY_KEY: string
-  // Define other environment variables here
+  // 更多环境变量...
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
+// 如果 ImportMetaEnv 增强不起作用，请确保在 vite-env.d.ts 中没有任何 import 语句。
